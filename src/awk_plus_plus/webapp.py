e6b7028a276a1a055ef7593e1f5a4fc1befc4011
sanchezcarlosjr <24639141+sanchezcarlosjr@users.noreply.github.com>
2024-09-19 08:27:59 -0700

    feat: improve webapp to support dataframes

--- src/awk_plus_plus/webapp.py
+++ src/awk_plus_plus/webapp.py
@@ -25,10 +25,26 @@ def predict(expression):
 def predict_as_dataframe(expression):
     return pd.DataFrame.from_records(predict(expression))
 
+
 with gr.Blocks(theme=theme, title="awk_plus_plus", css=css, analytics_enabled=False) as demo:
     gr.Markdown("# Greetings from Awk Plus Plus!")
     expression = gr.Code(label="Expression", language="json")
-    gr.Examples([""" i("sql:SELECT * FROM VALUES (1)")  """, "Systems"], inputs=[expression])
+    gr.Examples([
+        """i("sql:")""",
+        """i("sql:SELECT *  FROM (VALUES (1, 'red')) AS colors(id, color)")""",
+        """
+[
+      i("https://rickandmortyapi.com/api/character/%s" % id) + 
+      {image: i(super.image+"?awk_download=data/"+std.strReplace(std.asciiLower(super.name), " ", "_")+".jpeg")} + 
+      {episode:: [i(episode) for episode in super.episode]} +
+      {location:: ""} +
+      {origin:: ""}
+     for id in std.range(1,10)
+]
+        """
+     ],
+                inputs=[expression]
+     )
     submit_btn = gr.Button("Interpret")
     result = gr.DataFrame()
     submit_btn.click(fn=predict_as_dataframe, inputs=expression, outputs=result, api_name="interpret")
